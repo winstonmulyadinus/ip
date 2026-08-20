577c17bf838b7781373ec45dc2acde1bee781832
Winston <254264613+winstonmulyadinus@users.noreply.github.com>
2026-08-21 00:52:53 +0800

    Changed to Listing Bot
    The bot now stores a list of the users inputs, then displays the list
    when input = "list".

--- src/main/java/Carlo.java
+++ src/main/java/Carlo.java
@@ -2,35 +2,52 @@ import java.util.Scanner;
 
 public class Carlo {
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n" +
-                "  ██████╗                      \n" +
-                " ██╔════╝                      \n" +
-                " ██║       █████╗ ██████╗ ██╗      ██████╗ \n" +
-                " ██║      ██╔══██╗██╔══██╗██║     ██╔═══██╗\n" +
-                " ██║      ███████║██████╔╝██║     ██║   ██║\n" +
-                " ██║      ██╔══██║██╔══██╗██║     ██║   ██║\n" +
-                " ╚███████╗██║  ██║██║  ██║███████╗╚██████╔╝\n" +
-                "  ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝ \n" +
-                "Cheers! My name is Carlo!\n" +
-                "How may I help you?\n" +
+        String greeting = """
+                ____________________________________________________________
+                  ██████╗                     \s
+                 ██╔════╝                     \s
+                 ██║       █████╗ ██████╗ ██╗      ██████╗\s
+                 ██║      ██╔══██╗██╔══██╗██║     ██╔═══██╗
+                 ██║      ███████║██████╔╝██║     ██║   ██║
+                 ██║      ██╔══██║██╔══██╗██║     ██║   ██║
+                 ╚███████╗██║  ██║██║  ██║███████╗╚██████╔╝
+                  ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝\s
+                Cheers! My name is Carlo!
+                I can help you to list down anything!
+                ____________________________________________________________
+                Input your items in order!
+                Say 'list' and I will show you your list!
+                When you're done, just say 'bye'!
+                ____________________________________________________________
+                """;
+        String farewell = "Byeeee! Love always! <3\n" +
                 "____________________________________________________________\n";
-        String farewell = "Byeeee! Love always!\n" +
-                "____________________________________________________________\n";
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println(greeting);
 
-        try (Scanner scanner = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) {bye
             while (scanner.hasNextLine()) {
                 String command = scanner.nextLine();
 
                 System.out.println("____________________________________________________________");
 
                 if (command.equals("bye")) {
-                    System.out.println("Byeee! Love always!");
-                    System.out.println("____________________________________________________________");
+                    System.out.println(farewell);
                     break;
+                } else if (command.equals("list")) {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                } else {
+                    tasks[taskCount] = command;
+                    taskCount++;
+                    System.out.println(" added: " + command);
                 }
 
-                System.out.println(" " + command);
+
+                // System.out.println(" " + command);
                 System.out.println("____________________________________________________________");
             }
         }
