69d30815deaf78fbd5fa1d2ff8f3d473e347459a
Winston <254264613+winstonmulyadinus@users.noreply.github.com>
2026-08-21 00:36:05 +0800

    Added Scanner for bot to echo input
    the bot now echos every input! terminate the bot by inputting "bye",
    getting a nice farewell message before the code exits.

--- src/main/java/Carlo.java
+++ src/main/java/Carlo.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Carlo {
     public static void main(String[] args) {
-        String banner = "____________________________________________________________\n" +
+        String greeting = "____________________________________________________________\n" +
                 "  ██████╗                      \n" +
                 " ██╔════╝                      \n" +
                 " ██║       █████╗ ██████╗ ██╗      ██████╗ \n" +
@@ -11,9 +13,26 @@ public class Carlo {
                 "  ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝ \n" +
                 "Cheers! My name is Carlo!\n" +
                 "How may I help you?\n" +
-                "____________________________________________________________\n" +
-                "Byeeee! Love always!\n" +
                 "____________________________________________________________\n";
-        System.out.println(banner);
+        String farewell = "Byeeee! Love always!\n" +
+                "____________________________________________________________\n";
+        System.out.println(greeting);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String command = scanner.nextLine();
+
+                System.out.println("____________________________________________________________");
+
+                if (command.equals("bye")) {
+                    System.out.println("Byeee! Love always!");
+                    System.out.println("____________________________________________________________");
+                    break;
+                }
+
+                System.out.println(" " + command);
+                System.out.println("____________________________________________________________");
+            }
+        }
     }
 }
